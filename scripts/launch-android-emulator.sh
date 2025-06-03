c22a3c451db939a3e7f5605eee0bca3d3ccb1440
Woodrow Melling <woodymelling@gmail.com>
2025-06-02 17:29:17 -0700

    Launch Android Emulator script

---
+++ scripts/launch-android-emulator.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# Required parameters:
+# @raycast.schemaVersion 1
+# @raycast.title Launch Android Emulator
+# @raycast.mode silent
+
+# Optional parameters:
+# @raycast.icon 📱
+
+# Documentation
+# @raycast.description Launch the default android phone, currently: Medium_Phone_API_36
+# @raycast.author Woody
+
+/Users/woody/Library/Android/sdk/emulator/emulator -avd Medium_Phone_API_36
